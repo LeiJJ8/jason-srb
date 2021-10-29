7054daa104d369e14ab130109b8dd004532e347a
LeiJJ <19176587511@163.com>
2021-10-29 11:42:32 +0800

    添加统一返回结果

--- service-core/src/main/java/com/happy/srb/core/controller/admin/AdminIntegralGradeController.java
+++ service-core/src/main/java/com/happy/srb/core/controller/admin/AdminIntegralGradeController.java
@@ -31,12 +31,49 @@ public class AdminIntegralGradeController {
 
     @ApiOperation(value = "根据id删除积分等级",notes = "逻辑删除")
     @DeleteMapping("/remove/{id}")
-    public Result removeById(@ApiParam(value = "数据id",required = true,example = "1") @PathVariable("id") Long id){
+    public Result removeById(@ApiParam(value = "数据id",required = true,example = "1")
+                                 @PathVariable("id") Long id){
         boolean flag = integralGradeService.removeById(id);
         if(flag){
             return Result.ok().message("删除成功");
         }else{
             return Result.error().message("删除失败");
+        }
+    }
+
+    @ApiOperation(value = "新增积分等级")
+    @PostMapping("/save")
+    public Result save(@ApiParam(value = "积分等级对象",required = true)
+                           @RequestBody IntegralGrade integralGrade){
+        boolean save = integralGradeService.save(integralGrade);
+        if(save){
+            return Result.ok().message("保存成功");
+        }else {
+            return Result.error().message("保存失败");
+        }
+    }
+
+    @ApiOperation("根据id查询积分等级")
+    @GetMapping("/get/{id}")
+    public Result getById(@ApiParam(value = "查询数据id",required = true,example = "1")
+                              @PathVariable("id") Integer id){
+        IntegralGrade integralGrade = integralGradeService.getById(id);
+        if(integralGrade != null){
+            return Result.ok().data("integralGrade",integralGrade);
+        }else{
+            return Result.error().message("数据不存在");
+        }
+    }
+
+    @ApiOperation("更新积分等级")
+    @PutMapping("/update")
+    public Result updateById(@ApiParam(value = "积分等级对象",required = true)
+                               @RequestBody IntegralGrade integralGrade){
+        boolean update = integralGradeService.updateById(integralGrade);
+        if(update){
+            return Result.ok().message("更新成功");
+        }else{
+            return Result.error().message("更新失败");
         }
     }
 }
